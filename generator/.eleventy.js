d2afe8505598bb5e6b0108781ac2c2e2c489f350
Clovis <clovis@synapse-medicine.com>
2024-02-11 15:56:00 +0100

    feat: add 11ty config

--- generator/.eleventy.js
+++ generator/.eleventy.js
@@ -3,4 +3,12 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("{,!(_site)/**/}ressources/*.jpe?g");
     eleventyConfig.addPassthroughCopy("{,!(_site)/**/}ressources/*.gif");
     eleventyConfig.addPassthroughCopy("{,!(_site)/**/}ressources/*.webp");
+
+    return {
+      dir: {
+        input: "/var/www/html/blogs/",
+        output: "/var/www/html/",
+        includes: "/noircir/generator/_includes"
+      }
+    }
   };
