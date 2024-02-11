055a551ff50771092cece6176d99a14f6299dd1b
Clovis <clovis@synapse-medicine.com>
2024-02-11 15:12:50 +0100

    chore: try working startup

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 service nginx start
-npx @11ty/eleventy --config=/noircir/generator/.eleventy.js --incremental --input=/var/www/html/blogs/ --output=/var/www/html/ --watch
+npx @11ty/eleventy --incremental --input=/var/www/html/blogs/ --output=/var/www/html/ --watch
