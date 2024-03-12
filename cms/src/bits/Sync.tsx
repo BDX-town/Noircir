1a06c6d37bbf18b4664b669c34e4210a55768a83
Clovis <clovis@synapse-medicine.com>
2024-03-12 09:30:41 +0100

    feat: better sync message and process

--- cms/src/bits/Sync.tsx
+++ cms/src/bits/Sync.tsx
@@ -4,14 +4,34 @@ import fr from './Sync.fr-FR.i18n.json';
 import { useTranslations } from '@bdxtown/canaille';
 import { subscribe, unsubscribe } from '../sw/sw';
 import { SW_MESSAGES } from '../sw/messages';
-import { IconWifi, IconWifiOff } from '@tabler/icons-react';
+import { IconLoader3, IconWifi, IconWifiOff } from '@tabler/icons-react';
 import { useAppContext } from '../data/AppContext';
+import { flushSync } from 'react-dom';
+
+const Waiting = ({ queue, online }: { queue: number, online: boolean }) => {
+    const { T } = useTranslations("Sync", { 'fr-FR': fr });
+    return (
+        (!online || queue > 0) ? (
+            <T number={queue}>queued-operation</T>
+        ) : (
+            <T>in-sync</T>
+        )
+    )
+}
 
 export const Sync = () => {
     const { T } = useTranslations("Sync", { 'fr-FR': fr });
-    const { online } = useAppContext();
+    const { online, actions } = useAppContext();
+    const { refresh } = actions;
+    const [syncing, setSyncing] = React.useState(false);
+    const desync = React.useRef(false);
 
     const [queue, setQueue] = React.useState(0);
+    const [errors, setErrors] = React.useState<unknown[]>([]);
+
+    React.useEffect(() => {
+        if(queue > 0) desync.current = true;
+    }, [queue])
 
     const onMessage = React.useCallback(({ type, data}: { type: SW_MESSAGES, data: unknown}) => {
         if(type === SW_MESSAGES.QUEUE_UPDATE) {
@@ -19,8 +39,10 @@ export const Sync = () => {
             setQueue(queue.length);
         } else if(type === SW_MESSAGES.QUEUED_REQUEST_ERROR) {
             console.log('queue error', data);
+            // we ensure errors are immediatly processed
+            flushSync(() => setErrors([...errors, data]));
         }
-    }, []);
+    }, [errors]);
 
     React.useEffect(() => {
         subscribe(onMessage);
@@ -28,19 +50,46 @@ export const Sync = () => {
         return () => unsubscribe(onMessage);
     }, [onMessage]);
 
+    const sync = React.useCallback(async () => {
+        setSyncing(true);
+        try {
+            await refresh();
+        } finally {
+            desync.current = false;
+            setSyncing(false);
+        }
+    }, [refresh]);
+
+    React.useEffect(() => {
+        if(online && !syncing && desync.current && errors.length === 0 && queue === 0) {
+            sync();
+        }
+    }, [errors.length, online, queue, sync, syncing]);
+
     return (
-        <div className="flex items-center gap-2">
+        <div className="text-xs opacity-60">
+            <div className='flex items-center gap-1'>
+                {
+                    online ? (
+                        (syncing || queue > 0) ? (
+                            <IconLoader3 className='animate-spin' />
+                        ) : (
+                            <IconWifi className='text-green-500' />
+                        )
+                    ) : (
+                        <IconWifiOff className='text-red-500' /> 
+                    )
+                }
+                <Waiting queue={queue} online={online}/>
+            </div>
             {
-                online ? (
-                    <>
-                        <IconWifi className='opacity-20' />
-                    </>
-                ) : (
-                    <>
-                        <IconWifiOff /> <span className='text-sm'><T number={queue}>queued-operation</T></span>
-                    </>
+                !online && (
+                    <div>
+                        <T>offline</T>
+                    </div>
                 )
             }
+ 
         </div>
     )
 };
